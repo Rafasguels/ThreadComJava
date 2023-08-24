e9d2921fc679c267d4becfc6086d5953548b894e
Rafasguels <Rafael.vieira159@outlook.com>
2023-08-23 23:33:20 -0300

    Exercicios de fixação

---
+++ src/ThreadNaPratica.java
@@ -0,0 +1,17 @@
+
+public class ThreadNaPratica {
+
+	public static void main(String[] args) throws InterruptedException  {
+		
+		for (int pos = 0 ; pos < 10 ; pos++); {
+		
+		 /*Da um tempo*/
+		/*execultado atividade pedida*/
+		System.out.println("Teste de milisegundo usando Thread");
+			
+		Thread.sleep(2000);		
+		
+		}
+	}
+
+}

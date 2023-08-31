b68005c82dad9d396b227151f379ac52e113ed66
Rafasguels <Rafael.vieira159@outlook.com>
2023-08-31 19:04:15 -0300

    exercicios de fixação4

--- src/TelaTimeThread.java
+++ src/TelaTimeThread.java
@@ -4,6 +4,10 @@ import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
@@ -11,6 +15,7 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.plaf.DimensionUIResource;
+import javax.xml.crypto.Data;
 
 public class TelaTimeThread extends JDialog {
 	
@@ -25,6 +30,52 @@ public class TelaTimeThread extends JDialog {
 	
 	private JButton jButton = new JButton("start");
 	private JButton jButton2 = new JButton("stop");
+	
+	
+	private Runnable thread1 = new Runnable() {
+		
+		@Override
+		public void run() {
+			while (true) {/* fia sempre rodando as horas*/
+				mostraTempo.setText(new SimpleDateFormat("dd/MM/yyyy hh:mm:ss").
+						format(Calendar.getInstance().getTime()));
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			
+		}
+	};
+	
+	
+	private Runnable thread2 = new Runnable() {
+		
+		@Override
+		public void run() {
+			while (true) {/* fia sempre rodando as horas*/
+				mostraTempo2.setText(new SimpleDateFormat("dd/MM/yyyy hh:mm:ss").
+						format(Calendar.getInstance().getTime()));
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			
+		}
+	};
+	
+	
+	private Thread thread1Time;
+	private Thread thread2Time;
+	
+	
+	
+	
 	
 
 	public TelaTimeThread() { /*Construtor. execulta o que tiver dentro no momento da abertura ou execução*/
@@ -72,8 +123,56 @@ public class TelaTimeThread extends JDialog {
 		jButton2.setPreferredSize(new Dimension(92, 25));
 		gridBagConstraints.gridx ++;
 		jpanel.add(jButton2, gridBagConstraints);
+
+		
+		jButton.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) { /*Execulta o clique */
+				
+				thread1Time = new Thread(thread1);
+				thread1Time.start();
+				
+				thread2Time = new Thread(thread2);
+				thread2Time.start();
+				
+				jButton.setEnabled(false);
+				jButton2.setEnabled(true);
+				
+				
+			}
+		});
 		
 		
+		jButton2.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) { /*Execulta o clique */
+				
+				thread1Time.stop();
+				thread2Time.stop();
+				
+				jButton.setEnabled(true);
+				jButton2.setEnabled(false);
+				
+				
+			}
+		});
+		
+		
+		jButton2.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		
+		
+		jButton2.setEnabled(false);
+		
+
 		add(jpanel, BorderLayout.WEST);
 		/*Sempre sera o ultimo comando*/
 		setVisible(true); /*Torna uma tela visivel para o usuario*/
